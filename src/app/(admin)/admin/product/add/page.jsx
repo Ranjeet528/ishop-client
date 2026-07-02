ca76d70af43db6d0ed195a499227f2f345e8aa5e
Ranjeet528 <Ranjeetgurjar528@gmail.com>
2026-07-03 01:58:12 +0530

    update

--- src/app/(admin)/admin/product/add/page.jsx
+++ src/app/(admin)/admin/product/add/page.jsx
@@ -1,7 +1,8 @@
 'use client'
 import react, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
-import { getCategories, getBrands, getColors } from "@/api/api-call";
+import {  getBrands, getColors } from "@/api/api-call";
+import { getCategories} from "@/api/server-api";
 import Select from 'react-select'
 import { client, notify } from "@/utils/helper";
 import { Editor } from 'primereact/editor';
